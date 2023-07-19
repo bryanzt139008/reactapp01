2b54fe4becdae4d247d422fc5a5ed01f69cdc543
Bryan Zapana <bryanbozt139008@gmail.com>
2023-07-18 19:00:43 -0500

    se agrego 3 componentes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import C01componente from './component/C01componente';
 import C02contador from './component/C02contador';
 import C03doblecontador from './component/C03doblecontador';
+import C04variable from './component/C04variable';
+import C05operador from './component/C05operador';
+import C06matriz from './component/C06matriz';
 
 function App() {
   return (
@@ -14,6 +17,12 @@ function App() {
       <C02contador />
       <h3>Tercer Componente</h3>
       <C03doblecontador />
+      <h1>Cuarto Componente</h1>
+      <C04variable xVariable= "hola mundo"/>
+      <h1>Quinto Componente</h1>
+      <C05operador/>
+      <h1>Sexto Componente</h1>
+      <C06matriz/>
 
     </div>
   );
